457968908cb7ae8622ad4127fa3dcf57b0bdc7db
harsh-185 <harsh.maheshwari180504@gmail.com>
2024-06-05 10:15:57 -0400

    Created Register screen, added navigation and formatted the structure of the documents

---
+++ src/screens/App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from "expo-status-bar";
+
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import EditDetails from "./EditDetails";
+import Login from "./Login";
+import Register1 from "./Register1";
+import Register2 from "./Register2";
+import HomePage from "./HomePage";
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: "Login" }}
+        />
+        <Stack.Screen
+          name="Register1"
+          component={Register1}
+          options={{ title: "Register" }}
+        />
+        <Stack.Screen
+          name="Register2"
+          component={Register2}
+          options={{ title: "Register" }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomePage}
+          options={{ title: "Home Page" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/screens/Register1.js
@@ -0,0 +1,229 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import {
+  Alert,
+  Button,
+  Image,
+  ScrollView,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+const logo = require("../images/logo.png");
+
+const sampleImage = require("../images/video.jpg");
+
+function Register1({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    if (
+      password.length >= 8 &&
+      confirmPassword.length >= 8 &&
+      password === confirmPassword
+    ) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [password, confirmPassword]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={logo} style={styles.image} resizeMode="contain" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={username}
+          onChangeText={setUsername}
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirm Password"
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
+      </View>
+      {/* <View>
+              <Pressable onPress={() => Alert.alert("Forget Password!")}>
+                  <Text style={styles.forgetText}>Forgot Password?</Text>
+              </Pressable>
+          </View> */}
+
+      <View style={styles.buttonView}>
+        <Pressable
+          style={isDisabled ? styles.disabledButton : styles.button}
+          onPress={() => {
+            navigation.navigate("Register2");
+            Alert.alert("Register clicked!");
+          }}
+          disabled={isDisabled}
+        >
+          <Text style={styles.buttonText}>Next</Text>
+        </Pressable>
+      </View>
+      <View>
+        <Text>{"\n"}</Text>
+      </View>
+      <Text style={styles.footerText}>Already have an Account?</Text>
+
+      <View style={styles.buttonView}>
+        <Pressable
+          style={styles.registerButton}
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </Pressable>
+      </View>
+
+      <View>
+        <Text>{"\n"}</Text>
+      </View>
+
+      <Text style={styles.footerText}>
+        By continuing, you agree to the Terms and Conditions.
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    paddingTop: 70,
+  },
+  image: {
+    height: 160,
+    width: "100%",
+  },
+  sampleImage: {
+    height: 100,
+    width: 100,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    textAlign: "center",
+    paddingVertical: 40,
+    color: "#001E44",
+  },
+  inputView: {
+    gap: 15,
+    width: "100%",
+    paddingHorizontal: 40,
+    marginBottom: 5,
+  },
+  input: {
+    height: 50,
+    paddingHorizontal: 20,
+    borderColor: "#001E44",
+    borderWidth: 1,
+    borderRadius: 7,
+  },
+  rememberView: {
+    width: "100%",
+    paddingHorizontal: 50,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginBottom: 8,
+  },
+  switch: {
+    flexDirection: "row",
+    gap: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  rememberText: {
+    fontSize: 13,
+  },
+  forgetText: {
+    fontSize: 11,
+    color: "red",
+  },
+  button: {
+    backgroundColor: "#001E44",
+    height: 45,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  disabledButton: {
+    backgroundColor: "#7a7a7a",
+    height: 45,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  registerButton: {
+    backgroundColor: "#96BEE6",
+    height: 45,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  buttonView: {
+    width: "100%",
+    paddingHorizontal: 50,
+  },
+  optionsText: {
+    textAlign: "center",
+    paddingVertical: 10,
+    color: "gray",
+    fontSize: 13,
+    marginBottom: 6,
+  },
+  mediaIcons: {
+    flexDirection: "row",
+    gap: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 23,
+  },
+  icons: {
+    width: 40,
+    height: 40,
+  },
+  footerText: {
+    textAlign: "center",
+    color: "gray",
+  },
+});
+
+export default Register1;
