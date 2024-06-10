4c6da55bc5a088d37d11805d3f488f1209620ffb
M-Fabrizzi <mfabrizzi37@gmail.com>
2024-06-10 10:24:27 -0400

    added TOS and privacy policy links

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -10,6 +10,7 @@ import {
   StyleSheet,
   Switch,
   Text,
+  Linking,
   TextInput,
   View,
 } from "react-native";
@@ -80,7 +81,15 @@ function Login({ navigation }) {
       </View>
 
       <Text style={styles.footerText}>
-        By continuing, you agree to the Terms and Conditions.
+        By clicking “Login” or “Register,” {"\n"}you agree to our
+        <Text style={{color: 'blue'}} onPress={() => Linking.openURL('https://www.pennstatehealth.org/privacy-legal-notices')}>
+          {" Terms of Service "}
+        </Text>
+         and
+        <Text style={{color: 'blue'}} onPress={() => Linking.openURL('https://www.pennstatehealth.org/sites/default/files/Privacy/561-103-Privacy-Notice-PSH-Rev-11-21.pdf')}>
+          {" Privacy Policy"}
+        </Text>
+        .
       </Text>
 
       <View>
