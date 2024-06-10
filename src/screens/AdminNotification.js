9d315586fa5e7fd925ec262fa4276c436696aaaf
M-Fabrizzi <mfabrizzi37@gmail.com>
2024-06-10 14:52:20 -0400

    notification updates allowing for templates

--- src/screens/AdminNotification.js
+++ src/screens/AdminNotification.js
@@ -24,20 +24,52 @@ const theme = {
 const AdminNotification = ({ navigation }) => {
   const [ageGroupValue, setAgeGroupValue] = useState(null);
   const [diagnosisValue, setDiagnosisValue] = useState(null);
+  const [notifTypeValue, setNotifTypeValue] = useState(null);
   const [ageGroup, setAgeGroup] = useState("");
+  const [researchPreset, onChangeResearch] = useState('Your congenital heart team has identified you as someone potentially eligible for a current study being conducted on congenital heart disease. Would you like to be contacted about this study?');
+  const [cathPreset, onChangeCath] = useState('You are scheduled for an upcoming heart catheterization at Penn State Health’s Congenital Heart Center. Enclosed, you will find instructions for this procedure:');
+  const [surgeryPreset, onChangeSurgery] = useState('You are scheduled for an upcoming heart surgery at Penn State Health’s Congenital Heart Center. Enclosed, you will find instructions for this procedure:');
 
   return (
     <Provider theme={theme}>
       <ScrollView>
         <View style={styles.container}>
           <TextInput style={styles.input} placeholder="Title:" />
-          <TextInput style={styles.input} placeholder="Description:" />
+          <Dropdown
+            style={[styles.dropdown]}
+            data={[
+              { label: "Research", value: "1" },
+              { label: "Catheterization", value: "2" },
+              { label: "Surgery", value: "3" },
+              { label: "Other", value: "4" }
+            ]}
+            labelField="label"
+            valueField="value"
+            placeholder={"Notification Type"}
+            value={notifTypeValue}
+            onChange={(item) => {
+              setNotifTypeValue(item.value);
+            }}
+          />
+          {notifTypeValue === "1" && (
+            <TextInput multiline style={styles.descInput} placeholder="Description:" value={researchPreset} onChangeText={onChangeResearch}/>
+          )}
+          {notifTypeValue === "2" && (
+            <TextInput multiline style={styles.descInput} placeholder="Description:" value={cathPreset} onChangeText={onChangeCath}/>
+          )}
+          {notifTypeValue === "3" && (
+            <TextInput multiline style={styles.descInput} placeholder="Description:" value={surgeryPreset} onChangeText={onChangeSurgery}/>
+          )}
+          {notifTypeValue === "4" && (
+            <TextInput multiline style={styles.descInput} placeholder="Description:"/>
+          )}
           <TextInput style={styles.input} placeholder="Contact:" />
           <Dropdown
             style={[styles.dropdown]}
             data={[
               { label: "Pediatric", value: "1" },
-              { label: "Adult", value: "2" },
+              { label: "Transition", value: "2" },
+              { label: "Adult", value: "3" },
             ]}
             labelField="label"
             valueField="value"
@@ -81,6 +113,14 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  descInput: {
+    height: 120,
     borderColor: "gray",
     borderWidth: 1,
     marginBottom: 12,
