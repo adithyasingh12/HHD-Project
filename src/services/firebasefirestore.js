0251abfab177c15c23e794162d5be1450e539f0b
Bradie Fernandez <bradiefuentes@Bradies-MacBook-Pro.local>
2024-06-17 14:56:53 -0400

    Hooked up backend for create new categories

--- src/services/firebasefirestore.js
+++ src/services/firebasefirestore.js
@@ -1,6 +1,6 @@
 import firebase from "@react-native-firebase/app";
 import firestore from "@react-native-firebase/firestore";
-
+import navigation from 'react-native'
 export const addUserData = async (uid, data) => {
   try {
     await firestore().collection("UserPost").doc(uid).set(data);
@@ -10,11 +10,29 @@ export const addUserData = async (uid, data) => {
   }
 };
 
-export const updateUserData = async (uid, data) => {
+export const addCategoryData = async (categoryData, selectedItems, navigation) => {
   try {
-    await firestore().collection("UserPost").doc(uid).update(data);
-    console.log("Document successfully written!");
+  
+    if (selectedItems.length === 0) {
+      alert('Please select at least one item.'); 
+      return false;
+    }
+     else {
+      if (selectedItems.includes('1')) {
+        await firestore().collection("Categories").doc("Child").collection(categoryData.title).add(categoryData);
+      }
+      if (selectedItems.includes('2')) {
+        await firestore().collection("Categories").doc("Adult").collection(categoryData.title).add(categoryData);
+      }
+      if (selectedItems.includes('3')) {
+        await firestore().collection("Categories").doc("Transition").collection(categoryData.title).add(categoryData);
+      }
+      alert('Category created successfully!'); 
+      return true;
+    }
   } catch (error) {
-    console.error("Error writing document: ", error);
+    console.error("Error creating category, please try again ", error);
+    throw error; 
   }
+  
 };
