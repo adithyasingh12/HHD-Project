97bc70bca9d7c30e90b36d5c1f96b3c25c2612cf
harsh-185 <harsh.maheshwari180504@gmail.com>
2024-06-21 14:27:19 -0400

    changed video

--- src/services/firebasestorage.js
+++ src/services/firebasestorage.js
@@ -1,43 +1,56 @@
-import storage from '@react-native-firebase/storage';
-import firestore from '@react-native-firebase/firestore';
+import storage from "@react-native-firebase/storage";
+import firestore from "@react-native-firebase/firestore";
 import CategoryContext from "../context/categoryContext";
+import uuid from "react-native-uuid";
 
-export const uploadVideo = async (localUri, fileName, description, category, thumbnail, videoType, ageGroup) => {
+export const uploadVideo = async (
+  localUri,
+  fileName,
+  description,
+  category,
+  thumbnail,
+  videoType,
+  ageGroup
+) => {
   try {
     const response = await fetch(localUri);
     const blob = await response.blob();
 
-    // Reference to the specific bucket
-    const storageRef = storage().refFromURL('gs://chc-app-cd5bd.appspot.com').child(`videos/${fileName}`);
-    await storageRef.put(blob);    
-      try {
-        // Upload completed successfully, now get the download URL
-        const downloadURL = await storageRef.getDownloadURL();
-        const videoData = {
-          name: fileName,
-          url: downloadURL,
-          timestamp: new Date(),
-          desc: description,
-          category: category,
-          thumbnail: thumbnail,
-          videoType: videoType,
-          ageGroup: ageGroup
-        };
+    const uid = uuid.v4();
 
-        for(let i = 0; i < category.length; i++){
-          const docRef = await firestore()
-          .collection('Categories')
+    // Reference to the specific bucket
+    const storageRef = storage()
+      .refFromURL("gs://chc-app-cd5bd.appspot.com")
+      .child(`videos/${uid}`);
+    await storageRef.put(blob);
+    try {
+      // Upload completed successfully, now get the download URL
+      const downloadURL = await storageRef.getDownloadURL();
+      const videoData = {
+        name: fileName,
+        url: downloadURL,
+        timestamp: new Date(),
+        desc: description,
+        category: category,
+        thumbnail: thumbnail,
+        videoType: videoType,
+        ageGroup: ageGroup,
+      };
+
+      for (let i = 0; i < category.length; i++) {
+        const docRef = await firestore()
+          .collection("Categories")
           .doc(videoType)
           .collection(ageGroup)
           .doc(category[i])
           .collection("VideoPost")
-          .add(videoData);
-        }
-      } catch (error) {
-        console.error('Error saving to Firestore:', error);
+          .doc(uid)
+          .set(videoData);
       }
+    } catch (error) {
+      console.error("Error saving to Firestore:", error);
+    }
   } catch (error) {
-    console.error('Error uploading video:', error);
+    console.error("Error uploading video:", error);
   }
 };
-
